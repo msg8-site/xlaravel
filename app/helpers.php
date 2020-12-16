ef90cf99f27c703e3fba3cab68cc3abf5c780bb4
msg8-site <tingyu@msg8.site>
2020-12-16 23:53:10 +0800

    框架默认名称长度修正

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,7 @@
 //全局常量定义
 define('COMM_SYSFLAG', basename((dirname(__DIR__))));  //定义系统标识名，用于session前缀等标识
 define('COMM_SECIP', false);  //是否为双层ip，默认 false 仅判断REMOTE_ADDR
-define('COMM_SYSNAME', 'xlaravel后台管理框架');  //平台名称
+define('COMM_SYSNAME', 'xlaravel后台框架');  //平台名称
 define('COMM_CODETYPE', 'imgcode');  //验证类型， google 为谷歌动态码， imgcode 为图形验证码
 
 //定义存储session的变量名称
