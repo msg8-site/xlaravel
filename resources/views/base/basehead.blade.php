14959e54b4635a0278cda719575f23dc619f6fca
msg8-site <tingyu@msg8.site>
2021-01-13 00:27:22 +0800

    修改在线静态资源库

--- resources/views/base/basehead.blade.php
+++ resources/views/base/basehead.blade.php
@@ -9,15 +9,15 @@
     <meta http-equiv="Cache-Control" content="no-siteapp" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ (''==($title??''))?COMM_SYSNAME:($title??'') }}</title>
-    <link rel="stylesheet" href="https://lib.baomitu.com/layui/2.5.7/css/layui.min.css">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/layui/2.5.7/css/layui.min.css">
     <link rel="stylesheet" href="/css/main.css">
     <!--[if lt IE 9]>
     <script src="https://cdn.staticfile.org/html5shiv/r29/html5.min.js"></script>
     <script src="https://cdn.staticfile.org/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="https://lib.baomitu.com/layui/2.5.7/layui.all.min.js"></script>
-    <script src="https://lib.baomitu.com/crypto-js/3.1.9/crypto-js.min.js"></script>
-    <script src="https://lib.baomitu.com/clipboard.js/2.0.6/clipboard.min.js"></script>
+    <script src="https://cdn.staticfile.org/layui/2.5.7/layui.all.min.js"></script>
+    <script src="https://cdn.staticfile.org/crypto-js/3.1.9/crypto-js.min.js"></script>
+    <script src="https://cdn.staticfile.org/clipboard.js/2.0.6/clipboard.min.js"></script>
     <script>
         var xlayui = layui;
         if (self.frameElement) {
