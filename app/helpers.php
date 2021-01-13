6ccab4a2f7b83e54306e280d42ee19406223e72a
msg8-site <tingyu@msg8.site>
2021-01-13 16:06:04 +0800

    配置参数拆分逻辑修改

--- app/helpers.php
+++ app/helpers.php
@@ -1,17 +1,5 @@
 <?php
 
-//全局常量定义
-define('COMM_SYSFLAG', basename((dirname(__DIR__))));  //定义系统标识名，用于session前缀等标识
-define('COMM_SECIP', env('COMM_SECIP', true));  //是否为双层ip，默认 false 仅判断REMOTE_ADDR
-define('COMM_DBLOCK', env('COMM_DBLOCK', false));  //数据库迁移回滚锁，防止误删除， true 允许回滚删除数据表，其他值如 false ，不允许回滚删除
-define('COMM_SYSNAME', env('COMM_SYSNAME', 'xlaravel后台框架'));  //平台名称
-define('COMM_CODETYPE', env('COMM_CODETYPE', 'imgcode'));  //验证类型， google 为谷歌动态码， imgcode 为图形验证码
-
-//定义存储session的变量名称
-define('SESS_USERNAME', COMM_SYSFLAG . '_username');  //用户名
-define('SESS_MORECKEY', COMM_SYSFLAG . '_moreclientkey');  //用户登录标识
-
-
 //ajax参数返回
 function cmd($c = -1, $m = '', $d = [])
 {
